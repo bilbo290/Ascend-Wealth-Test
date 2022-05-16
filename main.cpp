dd8e93e203db36902494efde8e5bc1345aba56c2
bilbo290 <70652519+bilbo290@users.noreply.github.com>
2022-05-16 15:54:25 +0700

    Add reading from file support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <fstream>
 
 class Rover
 {
@@ -17,7 +18,7 @@ public:
   }
   void moveForward()
   {
-    int xyDirection = findIndex(orientationMap, direction);
+    int xyDirection = findDirectionIndex(orientationMap, direction);
     std::string toMove;
     int toMoveSign;
     if (xyDirection % 2)
@@ -49,21 +50,21 @@ public:
       position[toMove] = position[toMove] + toMoveSign;
     }
   }
-  void changeDirection(char turn)
+  void changeDirection(std::string turn)
   {
-    int curr = findIndex(orientationMap, direction);
+    int curr = findDirectionIndex(orientationMap, direction);
     int turnValue;
-    if (turn == 'R')
+    if (turn == "R")
     {
       turnValue = 1;
     }
-    else if (turn == 'L')
+    else if (turn == "L")
     {
       turnValue = -1;
     }
     int newDirectionIndex = (((curr + turnValue) % 4) + 4) % 4;
     direction = orientationMap[newDirectionIndex];
-    std::cout << "New direction index :" << ((newDirectionIndex) % 4) << std::endl;
+    // std::cout << "New direction index :" << ((newDirectionIndex) % 4) << std::endl;
   }
   bool checkInRange(std::string toMove, int toMoveSign)
   {
@@ -83,7 +84,7 @@ public:
     }
     return true;
   }
-  int findIndex(std::string arr[], std::string key)
+  int findDirectionIndex(std::string arr[], std::string key)
   {
     int n = 4;
     for (int i = 0; i < n; i++)
@@ -98,35 +99,93 @@ public:
 
 int main()
 {
-  std::cout << "Initiaing Rover...\n";
-  std::cout << "Input map size : \n";
-  int operatingRange;
-  std::cin >> operatingRange;
-  Rover rover(operatingRange);
   while (true)
   {
 
-    std::cout << "1. Move forward \n2. Turn\n3. Exit \n";
-    char userInput;
-    std::cin >> userInput;
-
-    switch (userInput)
+    std::cout << "Input file [i], enter CLI mode [c], or exit [e]" << std::endl;
+    std::string inputType;
+    std::cin >> inputType;
+    if (inputType == "i" || inputType == "I")
     {
-    case '1':
-      rover.moveForward();
-      break;
-    case '2':
-      char turn;
-      std::cout << "Left for left, R for right : \n";
-      std::cin >> turn;
-      // std::cout << rover.direction << std::endl;
-      rover.changeDirection(turn);
-      break;
-    case '3':
+      std::string line;
+      std::string fileName;
+      std::cout << "Input file name with extension \n";
+      std::cin >> fileName;
+      std::ifstream myfile(fileName);
+      if (myfile.is_open())
+      {
+        std::getline(myfile, line);
+        int operatingRange = stoi(line);
+        Rover rover(operatingRange);
+        while (std::getline(myfile, line))
+        {
+          if (line == "F")
+          {
+            rover.moveForward();
+          }
+          else if (line == "L" || "R")
+          {
+            rover.changeDirection(line);
+          }
+          else
+          {
+            std::cout << "Invalid input, skip to next line \n";
+            continue;
+          }
+          std::cout << "Input : " << line << " -> " << rover.direction << rover.position["x"] << "," << rover.position["y"] << std::endl;
+        }
+        myfile.close();
+      }
+
+      else
+      {
+        std::cout << "Unable to open file";
+        system("pause");
+        return 0;
+      }
+    }
+    else if (inputType == "c" || inputType == "C")
+
+    {
+      std::cout << "Initiaing Rover...\n";
+      std::cout << "Input map size : \n";
+      int operatingRange;
+      std::cin >> operatingRange;
+      Rover rover(operatingRange);
+      while (true)
+      {
+        std::cout << "Move forward [F], Turn [L,R], Exit [e] \n";
+        std::string userInput;
+        std::cin >> userInput;
+
+        if (userInput == "F")
+        {
+          rover.moveForward();
+        }
+        else if (userInput == "L" || userInput == "R")
+        {
+          rover.changeDirection(userInput);
+        }
+        else if (userInput == "e" || "E")
+        {
+          return 0;
+        }
+        else
+        {
+          std::cout << "Wrong input \n";
+          continue;
+        }
+        std::cout << rover.direction << rover.position["x"] << "," << rover.position["y"] << std::endl;
+      }
+    }
+    else if (inputType == "e" || inputType == "E")
+    {
       return 0;
     }
-    std::cout << rover.direction << rover.position["x"] << "," << rover.position["y"] << std::endl;
+    else
+    {
+      std::cout << "Invalid input \n";
+      continue;
+    }
   }
-
-  system("pause"); // <----------------------------------
 }
